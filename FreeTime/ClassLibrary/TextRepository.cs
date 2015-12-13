596c0e02a9f1d5cf36d981a98a4793f475b727c0
RazvanHidan <razvan_hidan@yahoo.com>
2015-12-13 19:53:53 +0200

    Add and List (Stream)

---
+++ FreeTime/ClassLibrary/TextRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class TextRepository
+    {
+        private Stream stream;
+
+        public TextRepository(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public void Add(Activity activity)
+        {
+            StreamWriter sw = new StreamWriter(stream);
+            sw.WriteLine(string.Join(" ",activity.Read()));
+            sw.Flush();
+        }
+
+        public string List()
+        {
+            stream.Position = 0;
+            StreamReader sr = new StreamReader(stream);
+            return sr.ReadToEnd();
+        }
+    }
+}
--- FreeTime/UnitTestProject/UnitTest.cs
+++ FreeTime/UnitTestProject/UnitTest.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Should;
 using ClassLibrary;
+using System.Globalization;
 
 namespace UnitTestProject
 {
@@ -10,12 +11,27 @@ namespace UnitTestProject
     public class UnitTest
     {
         [TestMethod]
-        public void TestActivity()
+        public void ActivityCreate()
         {
             var activity = new Activity();
             activity.Create("Prima incercare");
             activity.Read().ShouldContain("Prima");
-            activity.Read().ShouldContain("12/13/2015");
+            string dateNow = DateTime.Now.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
+            activity.Read().ShouldContain(dateNow);
+        }
+
+        [TestMethod]
+        public void StreamAddAndList()
+        {
+            var text = new TextRepository(new MemoryStream());
+            var activity = new Activity();
+            activity.Create("primul test");
+            text.Add(activity);
+            activity.Create("testul 2");
+            text.Add(activity);
+            text.List().Contains("primul test").ShouldBeTrue();
+            text.List().Contains("testul 2").ShouldBeTrue();
+            text.List().Contains("testul 3").ShouldBeFalse();
         }
     }
 }
