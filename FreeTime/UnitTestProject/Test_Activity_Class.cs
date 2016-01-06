5a1aca9310e70c85fcfdab55dab77fde0541bad4
RazvanHidan <razvan_hidan@yahoo.com>
2016-01-06 19:25:40 +0200

    Test Unit

---
+++ FreeTime/UnitTestProject/Test_Activity_Class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+using ClassLibrary;
+using System.Globalization;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class Test_Activity_Class
+    {
+        [TestMethod]
+        public void Activity_Has_Value()
+        {
+            var activity = new Activity("Test");
+            activity.ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void Activity_List_Return_A_Value()
+        {
+            var activity = new Activity("Test");
+            activity.List().ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void Activity_Contain_Exact_Message()
+        {
+            var activity = new Activity("Test");
+            activity.List().ShouldContain("Test");
+        }
+
+        [TestMethod]
+        public void Activity_Implement_NowDate()
+        {
+            var activity = new Activity("Test");
+            string dateNow = DateTime.Now.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
+            activity.List().ShouldContain(dateNow);
+        }
+    }
+}
---
+++ FreeTime/UnitTestProject/Test_TextRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+using ClassLibrary;
+using System.Globalization;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class Test_Class
+    {
+        [TestMethod]
+        public void TextRepository_Initialization()
+        {
+            var text = new TextRepository(new MemoryStream());
+            text.ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void TextRepository_First_Add()
+        {
+            var text = new TextRepository(new MemoryStream());
+            var activity=new Activity("First add");
+            text.Add(activity);
+            text.ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void TextRepository_Add()
+        {
+            var text = new TextRepository(new MemoryStream());
+            var activity = new Activity("First add");
+            text.Add(activity);
+            activity = new Activity("Second add");
+            text.Add(activity);
+            text.ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void TextRepository_List_Return_A_Value()
+        {
+            var text = new TextRepository(new MemoryStream());
+            var activity = new Activity("First add");
+            text.Add(activity);
+            text.List().ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void TextRepository_List_Return_Contents()
+        {
+            var text = new TextRepository(new MemoryStream());
+            var activity = new Activity("First add");
+            text.Add(activity);
+            text.List().Contains("First add").ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void TextRepository_ListWeek()
+        {
+            var text = new TextRepository(new MemoryStream());
+            var activity=new Activity("primul test");
+            text.Add(activity);
+            activity=new Activity("testul 2");
+            text.Add(activity);
+            text.ListWeek().Contains("primul test").ShouldBeTrue();
+            text.ListWeek().Contains("testul 2").ShouldBeTrue();
+            text.ListWeek().Contains("testul 3").ShouldBeFalse();
+        }
+    }
+}
