a390064ef82da1f304647149e7193cef9b956656
RazvanHidan <razvan_hidan@yahoo.com>
2016-03-17 02:31:24 +0200

    Extension

---
+++ HistoryVS/HistoryVS/Result/Event.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell;
+using System.ComponentModel;
+
+namespace HistoryVS
+{
+    public class GenericEvent
+    {
+        #region Member variables
+        RunningDocumentInfo rdi;
+        //private string message;
+        private DateTime start, end;
+        private string docName;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Base class for all other RDT event wrappers.  Each event wrapper 
+        /// stores event-specific information and formats it for display
+        /// in the Properties window.
+        /// </summary>
+        /// <param name="rdt">Running Document Table instance</param>
+        public GenericEvent(RunningDocumentTable rdt, string message, uint cookie)
+        {
+            //this.message = message;
+            if (rdt == null || cookie == 0) return;
+
+            rdi = rdt.GetDocumentInfo(cookie);
+            docName = rdi.Moniker;
+        }
+        #endregion
+        #region Public properties
+        [DisplayName("Event Name")]
+        [Description("The name of the event.")]
+        [Category("Basic")]
+        public string EventTime
+        {
+            get { return (end - start).ToString(); }
+        }
+        static char[] slashDelim = { '\\' };
+        [DisplayName("Doc Name, Short")]
+        [Description("The short name of the document.")]
+        [Category("Basic")]
+        public string DocumentName
+        {
+            get { return docName; }
+        }
+        #endregion
+    }
+}
---
+++ HistoryVS/HistoryVS/Result/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace HistoryVS
+{
+    public class Options 
+    {
+
+        private readonly Stopwatch stopwatch;
+        public Options()
+        {
+            stopwatch = new Stopwatch();
+
+        }
+        
+        public void Start()
+        {
+            stopwatch.Restart();
+        }
+
+        public void Stop()
+        {
+            stopwatch.Stop();
+        }
+
+            public bool IsRunning
+        {
+            get { return stopwatch.IsRunning; }
+        }
+
+            public long MillisecondsElapsed
+        {
+            get { return stopwatch.ElapsedMilliseconds; }
+        }
+
+        public Options ContainedOptions
+        {
+            get
+            {
+                return this;
+            }
+        }
+    }
+}
